f6dde57c6c504ee173c75ef8c5757052f188b4c7
Javier Cuevas <javi@diacode.com>
2013-03-12 12:38:53 -0700

    Added titles and changed routes for contact

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Diacode13::Application.routes.draw do
   root :to => 'pages#index'
 
   get 'services' => 'pages#services'
-  get 'contact' => 'pages#contact'
   get "work" => "pages#work"
 
-  resources :inquiries, :only => [:new, :create]
+  resources :inquiries, path: 'contact', :only => [:new, :create], 
+            path_names: { :new => ''}
 end
