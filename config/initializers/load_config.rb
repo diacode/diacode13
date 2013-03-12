ef3d61d58bda84b80f9f4a9865b38bd3356c76a2
Javier Cuevas <javi@diacode.com>
2013-03-11 23:12:26 -0700

    Fixed error on config

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -6,7 +6,7 @@ ActionMailer::Base.smtp_settings = {
  :address => 'smtp.gmail.com',
  :port => 587,
  :domain => "diacode.com",
- :user_name => APP_CONFIG['gmail_user'],
+ :user_name => APP_CONFIG['gmail_username'],
  :password => APP_CONFIG['gmail_password'],
  :authentication => 'plain'
 }
